6dc698a8995dd33f497a90554dae53d19644ce8c
ITSimaoAbel <simao3cuco@gmail.com>
2024-08-14 23:52:14 +0200

    Update Aluno, dash and turma

--- src/components/dashboard/aside.jsx
+++ src/components/dashboard/aside.jsx
@@ -5,51 +5,51 @@ export const Aside = () => {
   return (
     <aside className="w-64 bg-gray-800 text-white h-full fixed top-0 left-0">
       <div className="p-4">
-        <h2 className="text-2xl font-bold mb-6">Admin Panel</h2>
+        <h2 className="text-2xl font-bold text-orange mb-6">Admin Panel</h2>
         <nav>
           <ul>
             <li className="mb-4">
-              <Link to="/" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/" className="block py-2 px-4 rounded hover:bg-orange">
                 Dashboard
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/#" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/#" className="block py-2 px-4 rounded hover:bg-orange">
                 Home
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/mostrar-aluno" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/mostrar-aluno" className="block py-2 px-4 rounded hover:bg-orange">
                 Alunos
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/mostrar-turma" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/mostrar-turma" className="block py-2 px-4 rounded hover:bg-orange">
                 Turmas
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/Avaliacao" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/Avaliacao" className="block py-2 px-4 rounded hover:bg-orange">
                 Avaliação
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/Notas" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/Notas" className="block py-2 px-4 rounded hover:bg-orange">
                 Lançar Notas
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/disciplina-page" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/disciplina-page" className="block py-2 px-4 rounded hover:bg-orange">
                 Disciplinas
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/classe-page" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/classe-page" className="block py-2 px-4 rounded hover:bg-orange">
                 Classes
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/#" className="block py-2 px-4 rounded hover:bg-gray-700">
+              <Link to="/#" className="block py-2 px-4 rounded hover:bg-orange">
                 Sair
               </Link>
             </li>
