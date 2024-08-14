ddc720c41fc50f674c68743fd206daec955c430c
ITSimaoAbel <simao3cuco@gmail.com>
2024-08-14 22:23:07 +0200

    Update avaliacao

--- src/pages/avaliacao.jsx
+++ src/pages/avaliacao.jsx
@@ -1,14 +1,12 @@
 import { FormAvalicacao } from "../components/avaliacao";
+import { Dashboard } from "../components/dashboard/admindash";
 
 export const Avaliacao = () => {
     return (
       <>
+        <Dashboard />
         <FormAvalicacao />  
       </>
-      // <div className="p-6">
-      //   <h1 className="text-3xl font-bold">Avaliacao Page</h1>
-      //   <p className="mt-4">This is the Avaliacao page.</p>
-      // </div>
     );
   }
   
