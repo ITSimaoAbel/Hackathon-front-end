53a271b2219c8c3beb5f2497ca8c82eccd99c234
yuran-nhassengo <yurannhassengo@gmail.com>
2024-08-15 06:55:44 +0200

    Add proutect-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
 import { route } from '../routes'
+import { AuthProvider } from './components/context'
 
 export const App = () => {
   return (    
+    <AuthProvider>
       <RouterProvider router={route} /> 
+      </AuthProvider>
   )
 }
 export default App;
