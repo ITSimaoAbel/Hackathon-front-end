3551aa4c72eb2423040b8ad2b09abd26a81a5322
ITSimaoAbel <simao3cuco@gmail.com>
2024-08-15 03:08:23 +0200

    Update pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,110 @@
-import { Dashboard } from "../components/dashboard/admindash";
+import React from 'react';
+import { Chart } from 'react-google-charts';
+import { Dashboard } from '../components/dashboard/admindash';
+
+export const data = [
+  ["Matéria", "Nível de Dificuldade"],
+  ["Matemática", 65], 
+  ["Português", 55], 
+  ["História", 70],  
+  ["Ciências", 60],  
+  ["Geografia", 50], 
+  ["Física", 75],    
+  ["Química", 68],   
+];
+
+export const optionsBar = {
+  chart: {
+    title: "Matérias com Maior Nível de Dificuldade",
+    subtitle: "Dificuldade média das matérias",
+  },
+  bars: 'horizontal',
+  colors: ['#f37221', '#d6bda4', '#3357FF', '#FF33A1', '#A1FF33', '#33FFF4', '#F4FF33'],
+  hAxis: {
+    title: "Nível de Dificuldade",
+    format: 'decimal',
+  },
+  vAxis: {
+    title: "Matérias",
+  },
+  legend: { position: 'none' },
+};
+
+export const optionsPie = {
+  title: "Distribuição do Nível de Dificuldade por Matéria",
+ 
+  slices: {
+    0: { offset: 0.05 }, 
+    1: { offset: 0.05 },
+    2: { offset: 0.05 },
+    3: { offset: 0.05 },
+    4: { offset: 0.05 },
+    5: { offset: 0.05 },
+    6: { offset: 0.05 },
+  },
+  colors: ['#f37221', '#d6bda4', '#3357FF', '#FF33A1', '#A1FF33', '#33FFF4', '#F4FF33'],
+};
 
 export const Home = () => {
-    return (
-      <>
-        <Dashboard />
-      </>
-    );
-}
+  const totalAlunos = 150;  
+  const totalTurmas = 12;   
+  const totalClasses = 8;   
+  const totalDisciplinas = 5; 
+
+  return (
+    <>
+      <Dashboard />
+      <div className="p-4">
+        <div className="ml-80 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="p-4 bg-white rounded shadow-2xl">
+            <h3 className="text-lg font-semibold mb-2">Total de Alunos</h3>
+            <p className="text-2xl font-bold">{totalAlunos}</p>
+          </div>
+
+          <div className="p-4 bg-white rounded shadow-lg">
+            <h3 className="text-lg font-semibold mb-2">Total de Turmas</h3>
+            <p className="text-2xl font-bold">{totalTurmas}</p>
+          </div>
+
+          <div className="p-4 bg-white rounded shadow-lg">
+            <h3 className="text-lg font-semibold mb-2">Total de Classes</h3>
+            <p className="text-2xl font-bold">{totalClasses}</p>
+          </div>
+
+          <div className="p-4 bg-white rounded shadow-lg">
+            <h3 className="text-lg font-semibold mb-2">Total de Disciplinas</h3>
+            <p className="text-2xl font-bold">{totalDisciplinas}</p>
+          </div>
+        </div>
+        
+        <div className="ml-80 mt-20 flex justify-center gap-56 items-center mt-8">
+          <div className="p-4 bg-white rounded shadow-lg" style={{ width: '400px', height: '400px' }}>
+            <h2 className="text-xl font-bold text-orange mb-4 text-center">Dificuldade das Matérias</h2>
+            <Chart
+              chartType="Bar"
+              width="100%"
+              height="100%"
+              data={data}
+              options={optionsBar}
+            />
+          </div>
+          
+          <div className="p-4 bg-white rounded shadow-lg" style={{ width: '400px', height: '400px' }}>
+            <h2 className="text-xl font-bold mb-4 text-orange text-center">Distribuição do Nível de Dificuldade</h2>
+            <Chart
+              chartType="PieChart"
+              width="100%"
+              height="100%"
+              data={data}
+              options={optionsPie}
+            />
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  );
+};
+
 
